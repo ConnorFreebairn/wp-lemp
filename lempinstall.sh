4089f4cc113a8923210e887d6bbe9cf895dba2d6
Connor Freebairn <connor.freebairn@outlook.com>
2021-01-23 18:20:01 +0000

    Updated

--- lempinstall.sh
+++ lempinstall.sh
@@ -16,46 +16,47 @@ apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
 add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://mirror.netinch.com/pub/mariadb/repo/10.4/ubuntu focal main'
 
 # Install base packages
-apt-get update; apt-get install -y build-essential curl nano wget lftp unzip bzip2 arj nomarch lzop htop openssl gcc git binutils libmcrypt4 libpcre3-dev make python3 python3-pip supervisor unattended-upgrades whois zsh imagemagick uuid-runtime net-tools
+apt-get update; apt-get install -y build-essential curl nano wget lftp unzip bzip2 arj nomarch lzop htop openssl gcc git binutils libmcrypt4 libpcre3-dev make python3 python3-pip supervisor unattended-upgrades whois zsh imagemagick uuid-runtime net-tools nginx
 
 # Set the timezone to UTC
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
-sudo apt install nginx -y
-sudo systemctl enable nginx
-sudo systemctl start nginx
+# Enable and start nginx
+systemctl enable nginx
+systemctl start nginx
 
-sudo ufw allow http
+# Set www-data as the owner
+chown www-data:www-data /usr/share/nginx/html -R
+chown www-data:www-data /var/www/ -R
 
-sudo chown www-data:www-data /usr/share/nginx/html -R
-sudo chown www-data:www-data /var/www/ -R
+# Install php
+apt install -y php7.4 php7.4-fpm php7.4-mysql php-common php7.4-cli php7.4-common php7.4-json php7.4-opcache php7.4-readline php7.4-mbstring php7.4-xml php7.4-gd php7.4-curl
 
-sudo apt install -y php7.4 php7.4-fpm php7.4-mysql php-common php7.4-cli php7.4-common php7.4-json php7.4-opcache php7.4-readline php7.4-mbstring php7.4-xml php7.4-gd php7.4-curl
+# Enable and start php-fpm
+systemctl enable php7.4-fpm
+systemctl start php7.4-fpm
 
-sudo systemctl enable php7.4-fpm
-sudo systemctl start php7.4-fpm
-
-#Install Composer
+# Install Composer
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 export COMPOSER_ALLOW_SUPERUSER=1;
 
-#Install and configure Memcached
+# Install and configure Memcached
 apt-get install -y memcached
 sed -i 's/-l 0.0.0.0/-l 127.0.0.1/' /etc/memcached.conf
 systemctl restart memcached
 
-#Update PHP CLI configuration
+# Update PHP CLI configuration
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.4/cli/php.ini
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.4/cli/php.ini
 sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.4/cli/php.ini
 sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.4/cli/php.ini
 
-#Configure sessions directory permissions
+# Configure sessions directory permissions
 chmod 733 /var/lib/php/sessions
 chmod +t /var/lib/php/sessions
 
-#Tweak PHP-FPM settings
+# Tweak PHP-FPM settings
 sed -i "s/error_reporting = .*/error_reporting = E_ALL \& ~E_NOTICE \& ~E_STRICT \& ~E_DEPRECATED/" /etc/php/7.4/fpm/php.ini
 sed -i "s/display_errors = .*/display_errors = Off/" /etc/php/7.4/fpm/php.ini
 sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.4/fpm/php.ini
@@ -63,7 +64,7 @@ sed -i "s/upload_max_filesize = .*/upload_max_filesize = 256M/" /etc/php/7.4/fpm
 sed -i "s/post_max_size = .*/post_max_size = 256M/" /etc/php/7.4/fpm/php.ini
 sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.4/fpm/php.ini
 
-#Tune PHP-FPM pool settings
+# Tune PHP-FPM pool settings
 sed -i "s/;listen\.mode =.*/listen.mode = 0666/" /etc/php/7.4/fpm/pool.d/www.conf
 sed -i "s/;request_terminate_timeout =.*/request_terminate_timeout = 60/" /etc/php/7.4/fpm/pool.d/www.conf
 sed -i "s/pm\.max_children =.*/pm.max_children = 70/" /etc/php/7.4/fpm/pool.d/www.conf
@@ -72,13 +73,13 @@ sed -i "s/pm\.min_spare_servers =.*/pm.min_spare_servers = 20/" /etc/php/7.4/fpm
 sed -i "s/pm\.max_spare_servers =.*/pm.max_spare_servers = 35/" /etc/php/7.4/fpm/pool.d/www.conf
 sed -i "s/;pm\.max_requests =.*/pm.max_requests = 500/" /etc/php/7.4/fpm/pool.d/www.conf
 
-#Tweak Nginx settings
+# Tweak Nginx settings
 sed -i "s/worker_processes.*/worker_processes auto;/" /etc/nginx/nginx.conf
 sed -i "s/# multi_accept.*/multi_accept on;/" /etc/nginx/nginx.conf
 sed -i "s/# server_names_hash_bucket_size.*/server_names_hash_bucket_size 128;/" /etc/nginx/nginx.conf
 sed -i "s/# server_tokens off/server_tokens off/" /etc/nginx/nginx.conf
 
-#Configure Gzip for Nginx
+# Configure Gzip for Nginx
 cat > /etc/nginx/conf.d/gzip.conf << EOF
 gzip_comp_level 5;
 gzip_min_length 256;
@@ -102,10 +103,10 @@ text/css
 text/plain;
 EOF
 
-#Install MariaDB (MySQL) and set a strong root password
-apt-get install -y mariadb-server;
+# Install MariaDB (MySQL) and set a strong root password
+apt-get install -y mariadb-server
 
-#Secure your MariaDB installation
+# Secure your MariaDB installation
 MYSQL_ROOT_PASSWORD=$(date +%s|sha256sum|base64|head -c 36) #openssl rand -hex >
 WP_PASSWORD=$(date +%s+%m|sha256sum|base64|head -c 16) #openssl rand -hex 12
 WPDB=$newdomain
@@ -124,7 +125,7 @@ echo $MYSQL_ROOT_PASSWORD
 echo $WP_PASSWORD
 
 
-#We will install phpMyAdmin using Composer as Ubuntu packages are no longer being maintained.
+# We will install phpMyAdmin using Composer as Ubuntu packages are no longer being maintained.
 mkdir -pv /var/www/
 cd /var/www
 composer create-project phpmyadmin/phpmyadmin
@@ -133,10 +134,10 @@ mysql -u root -pYOUR_ROOT_PASSWORD < /var/www/phpmyadmin/sql/create_tables.sql
 sed -i "s/\$cfg\['blowfish_secret'\] = '';.*/\$cfg\['blowfish_secret'\] = '$(uuidgen)';/" /var/www/phpmyadmin/config.inc.php
 mkdir -pv /var/www/phpmyadmin/tmp; chown www-data:www-data /var/www/phpmyadmin/tmp;
 
-#Symlink phpMyAdmin, create logs dir and set permissions and ownership on /var/www
+# Symlink phpMyAdmin, create logs dir and set permissions and ownership on /var/www
 ln -s /var/www/phpmyadmin/ /var/www/html/phpmyadmin;  mkdir -pv /var/www/logs;  chown www-data:www-data /var/www/html; chown www-data:www-data /var/www/logs; chown www-data:www-data /var/www; chmod -R g+rw /var/www;
 
-#Create Nginx virtual host config
+# Create Nginx virtual host config
 newdomain=""
 domain=$1
 rootPath=$2
@@ -255,15 +256,15 @@ else
         echo "New Virtual Host Created"
 fi
 
-#Symlink
+# Symlink
 sudo ln -s /etc/nginx/sites-available/$newdomain.conf /etc/nginx/sites-enabled/
 
 rm /etc/nginx/sites-enabled/default
 
-#Install Letsencrypt Certbot
+# Install Letsencrypt Certbot
 apt install -y python3-certbot-nginx
 
-#Restart PHP-FPM and Nginx
+# Restart PHP-FPM and Nginx
 systemctl restart php7.4-fpm; systemctl restart nginx;
 
 echo 'LEMP Stack has been Installed \nNow downloding latest wordpress'
