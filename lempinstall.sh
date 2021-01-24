d0a569bb5e7856cdfc7e78356582b79c8435f251
Connor Freebairn <connor.freebairn@outlook.com>
2021-01-24 09:11:52 +0000

    disable wordpressinstall service after script

--- lempinstall.sh
+++ lempinstall.sh
@@ -364,3 +364,5 @@ APT::Periodic::Download-Upgradeable-Packages "1";
 APT::Periodic::AutocleanInterval "7";
 APT::Periodic::Unattended-Upgrade "1";
 EOF
+
+systemctl disable wordpressinstall.service
