2145b69b47f01b18dcfac212639b5e8d08f1ecae
Connor Freebairn <connor.freebairn@outlook.com>
2021-01-24 09:39:55 +0000

    Update lempinstall.sh

--- lempinstall.sh
+++ lempinstall.sh
@@ -364,7 +364,3 @@ APT::Periodic::Download-Upgradeable-Packages "1";
 APT::Periodic::AutocleanInterval "7";
 APT::Periodic::Unattended-Upgrade "1";
 EOF
-
-systemctl disable wordpressinstall.service
-systemctl mask wordpressinstall.service
-reboot
